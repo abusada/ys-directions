f73d830741a106d9cc155132f1f856a64bd0943d
Faris Abusada <faris.a@yamsafer.me>
2016-08-13 00:34:19 +0300

    aight

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,46 @@ const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var buildBranch = require('gulp-build-branch');
+var git = require('gulp-git');
+var buildBranchName = 'dist';
 
-gulp.task('publish', ['build'], function() {
-  return buildBranch({
-    folder: 'dist',
-    branch: 'dist'
-  });
+gulp.task('publish', ['build', 'buildBranch'], function() {
+    git.checkout(buildBranchName, function(err) {
+        // if (err)
+        //   throw err;
+        // else git.push('origin', buildBranchName, function(err) {
+        //     if (err) throw err;
+        // });
+    });
 });
 
-gulp.task('copy', function () {
-  return gulp
-    .src(['./*', '!app'])
-    .pipe(gulp.dest('dist'));
+
+gulp.task('copy', function() {
+    return gulp
+        .src(['./*', '!app'])
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('buildBranch', function() {
+    return buildBranch({
+        folder: buildBranchName,
+        branch: buildBranchName
+    });
+})
 
 gulp.task('build', ['copy'], function() {
-  return gulp.src(['dist/*.html'])
-    .pipe($.sourcemaps.init())
-    .pipe($.if('*.html', $.crisper({
-        scriptInHead: false
-    })))
-    .pipe($.if('*.js', $.babel({
-        presets: ['es2015']
-    })))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('dist', {
-      overwrite: true
-    }));
+    return gulp.src(['dist/*.html'])
+        .pipe($.sourcemaps.init())
+        .pipe($.if('*.html', $.crisper({
+            scriptInHead: false
+        })))
+        .pipe($.if('*.js', $.babel({
+            presets: ['es2015']
+        })))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('dist', {
+            overwrite: true
+        }));
 });
 
 gulp.task('serve', function() {
