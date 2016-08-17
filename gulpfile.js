30d917475bee0387c4ee7c7860bdb536d10db040
Faris Abusada <faris.a@yamsafer.me>
2016-08-17 12:59:48 +0300

    fix build process

--- gulpfile.js
+++ gulpfile.js
@@ -6,65 +6,63 @@ var buildBranch = require('gulp-build-branch');
 var buildBranchName = 'dist';
 
 gulp.task('publish', [
-    'build',
-    'buildBranch'
-  ], function() {
-    git.checkout(buildBranchName, err => {
-        if (err)
-          throw err;
-        git.push('origin', buildBranchName, err => {
-            if (err)
-              throw err;
-            git.checkout('master');
-        });
+  'build',
+  'buildBranch'
+], function() {
+  git.checkout(buildBranchName, err => {
+    if (err)
+      throw err;
+    git.push('origin', buildBranchName, err => {
+      if (err)
+        throw err;
+      git.checkout('master');
     });
+  });
 });
 
-
 gulp.task('copy', function() {
-    return gulp
-        .src(['./*', '!app'])
-        .pipe(gulp.dest('dist'));
+  return gulp.src(['*.*','demo/**/*','test/**/*'], {base: '.'})
+    .pipe(gulp.dest(buildBranchName));
 });
 
 gulp.task('buildBranch', function() {
-    return buildBranch({
-        folder: buildBranchName,
-        branch: buildBranchName
-    });
+  return buildBranch({
+    folder: buildBranchName,
+    branch: buildBranchName
+  });
 })
 
 gulp.task('build', ['copy'], function() {
-    return gulp.src(['dist/*.html'])
+  return gulp.src(['dist/*.{html, js}'])
         .pipe($.sourcemaps.init())
         .pipe($.if('*.html', $.crisper({
-            scriptInHead: false
+          scriptInHead: false
         })))
         .pipe($.if('*.js', $.babel({
-            presets: ['es2015']
+          presets: ['es2015']
         })))
         .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest('dist', {
-            overwrite: true
+        .pipe(gulp.dest(buildBranchName, {
+          overwrite: true
         }));
 });
 
 gulp.task('serve', function() {
-    browserSync.create();
-    browserSync({
-        logLevel: "debug",
-        logConnections: true,
-        logSnippet: true,
-        port: 5000,
-        notify: true,
-        server: {
-            baseDir: ['./'],
-            routes: {
-                "/bower_components": "app/bower_components"
-            }
-        }
-    });
-    gulp.watch([
-        '**/*.html'
-    ]).on('change', browserSync.reload);
+  browserSync.create();
+  browserSync({
+    logLevel: 'debug',
+    logConnections: true,
+    logSnippet: true,
+    port: 5000,
+    notify: true,
+    server: {
+      baseDir: ['./'],
+      routes: {
+        '/bower_components': 'app/bower_components'
+      }
+    }
+  });
+  gulp.watch([
+    '**/*.html'
+  ]).on('change', browserSync.reload);
 });
